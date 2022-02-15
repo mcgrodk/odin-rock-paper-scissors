dca2047912694fd36f6c2aa5a6607acbd237437f
Kyle <mcgroderk@gmail.com>
2022-02-15 10:24:32 +0900

    Added working computerPlay function

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function computerPlay() {
-    return Math.random()
-}
-
+    let number = Math.floor(Math.random()*10);
+    if (number <= 3) {
+        return "Rock";
+    }
+    else if (number > 3 && number <= 6) {
+        return "Paper";
+    }
+    else (number > 6) 
+        return "Scissors";
+    }
 console.log(computerPlay());
