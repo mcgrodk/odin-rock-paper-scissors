4250e043870215a92a40eb5f883958279dee9912
Kyle <mcgroderk@gmail.com>
2022-02-18 16:05:48 +0900

    Added stylesheet, added prompt for player input

--- script.js
+++ script.js
@@ -10,23 +10,24 @@ function computerPlay() {
         return "scissors";
     }
 
+
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
+    if (playerSelection == "rock" && computerSelection == "paper") {
         return "Sorry, you lose! Paper beats Rock. (┛✧Д✧))┛彡┻━┻"
-    } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") {
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
         return "You win! Rock beats Scissors. ᕕ( ᐛ )ᕗ"
     } 
-    else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") {
+    else if (playerSelection == "paper" && computerSelection == "rock") {
         return "You win! Paper beats Rock. ᕕ( ᐛ )ᕗ"
     } 
-    else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors") {
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
         return "Sorry, you lose! Scissors beats Paper. (┛✧Д✧))┛彡┻━┻"
-    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock") {
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
         return "Sorry, you lose! Rock beats Scissors. (┛✧Д✧))┛彡┻━┻"
-    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
         return "You win! Scissors beats Paper. ᕕ( ᐛ )ᕗ"
     } 
-    else if (playerSelection.toLowerCase() == computerSelection) {
+    else if (playerSelection == computerSelection) {
         return "It's a draw! Try again. ¯\_(ツ)_/¯"
     }
     else return "That is not a valid input, stop cheating! (; ･`д･´)​"
@@ -39,7 +40,7 @@ function game() {
     }
 }
 
-const playerSelection = "rock";
+const playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase();
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
 
@@ -47,6 +48,6 @@ console.log(playRound(playerSelection, computerSelection));
 If player wins, print a win message; if player loses, print lose message; if a draw, print draw message
 Start from base count Wins: 0 and Losses: 0
 If player wins, add 1 to Wins; if player loses, add 1 to losses
-A draw does not add to win or loss count, but still counts as a round played
+A draw does not add to win or loss count, and does not count toward 5 rounds
 After 5 rounds played, display final win/loss count with final message
 */
