b2923624674af8f0b21a861cf5ac2d38d6545d35
Kyle <mcgroderk@gmail.com>
2022-02-28 09:58:00 +0900

    Modified existing functions, added working game function (First full working commit)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Generates random number to determine computer's choice
+// Generates random number to determine computer's each round
 function computerPlay() {
   const number = Math.floor(Math.random()*3) + 1;
   if (number == 1) {
@@ -11,62 +11,44 @@ function computerPlay() {
       return "scissors";
   }
 
-//Gets player's choice from browser prompt, case-insensitive
+// Gets the player's choice for each round from text prompt
 function playerPlay() {
 const playerInput = prompt('Rock, Paper, or Scissors?').toLowerCase();
 return playerInput;
 }
 
-//Gets return value from player choice vs computer choice
-function playRound(playerSelection, computerSelection) {
-if (playerSelection == computerSelection) {
-  return "draw";
-} else if (playerSelection == "rock") {
-  if (computerSelection == "scissors") {
-    return "player wins";
-  } else return "computer wins";
-} else if (playerSelection == "paper") {
-  if (computerSelection == "rock") {
-    return "player wins";
-  } else return "computer wins";
-} else if (playerSelection == "scissors") {
-  if (computerSelection == "paper") {
-    return "player wins";
-  } else return "computer wins";
-} 
-}
-
-const playerSelection = playerPlay();
-const computerSelection = computerPlay();
-
 let playerScore = 0;
 let computerScore = 0;
 
-//Plays five rounds and returns win/lose message for each with player and computer score
-function game() {
-for (let i = 0; i < 5; i++) {
+// Plays a single round between player and computer, then logs the win-loss-draw result
+function playRound(playerSelection, computerSelection) {
 
-let result = playRound(playerSelection, computerSelection);
-if (result == "player wins") {
+playerSelection = playerPlay();
+computerSelection = computerPlay();
+
+if (playerSelection == computerSelection) {
+  console.log(`It's a draw! Try again. ¯\_(ツ)_/¯ \nYour Score: ${playerScore} | Computer's Score: ${computerScore}`);
+} else if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper") {
   playerScore++;
-  console.log(`You win, ${playerSelection} beats ${computerSelection}! ᕕ( ᐛ )ᕗ \nYour Score: ${playerScore} | Computer's Score: ${computerScore}`);
-} else if (result == "computer wins") {
+  console.log(`You win, ${playerSelection} beats ${computerSelection}! ᕕ( ᐛ )ᕗ \nYour Score: ${playerScore} | Computer's Score: ${computerScore}`)
+} else if (playerSelection == "rock" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "rock") {
   computerScore++;
   console.log(`You lose, ${computerSelection} beats ${playerSelection}! (┛✧Д✧))┛彡┻━┻ \nYour Score: ${playerScore} | Computer's Score: ${computerScore}`);
-} else if (result == "draw") {
-  console.log(`It's a draw! Try again. ¯\_(ツ)_/¯ \nYour Score: ${playerScore} | Computer's Score: ${computerScore}`);
-} else console.log(`That is not a valid input, stop cheating! (; ･\`д･´) \nYour Score: ${playerScore} | Computer's Score: ${computerScore}`);
+} else if (playerSelection != "rock" || playerSelection != "paper" || playerSelection != "scissors") {
+  console.log(`That is not a valid input, stop cheating! (; ･\`д･´) \nYour Score: ${playerScore} | Computer's Score: ${computerScore}`)
+}
 }
 
+// Plays 5 rounds, then returns an alert with winner and results
+function game() {
+for (let i= 0; i < 5; i++) {
+  playRound();
 }
+if (playerScore > computerScore) {
+  alert(`You\'re the Rock-Paper-Scissors champion! o͡͡͡͡͡͡͡͡͡͡͡͡͡͡╮(｡ᐤヮᐤ｡)╭o͡͡͡͡͡͡͡͡͡͡͡͡͡͡' \n Final Score: You: ${playerScore} | Computer: ${computerScore}`);
+} else if (playerScore < computerScore) {
+  alert(`You lost to the computer! How embarrassing. \(´Д｀) \n Final Score: You: ${playerScore} | Computer: ${computerScore}`);
+} else alert(`There's no winner here! Maybe try again? 「\(°ヘ°) \n Final Score: You: ${playerScore} | Computer: ${computerScore}`)
+}
+
 game();
-
-
-
-/* Use "game" function to play five rounds
-If player wins, print a win message; if player loses, print lose message; if a draw, print draw message
-Start from base count Wins: 0 and Losses: 0
-If player wins, add 1 to Wins; if player loses, add 1 to losses
-A draw does not add to win or loss count, and does not count toward 5 rounds
-After 5 rounds played, display final win/loss count with final message
-*/
