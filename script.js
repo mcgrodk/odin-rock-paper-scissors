26f960df9b1ddafe34bed01ff296006f2a5a8979
Kyle <mcgroderk@gmail.com>
2022-03-24 15:41:33 +0900

    Added working button events, results display for rounds

--- script.js
+++ script.js
@@ -13,63 +13,50 @@ function computerPlay() {
 let playerScore = 0;
 let computerScore = 0;
 
-// Add event listeners to each button
-// Clicked button value becomes playerSelection
-// From there, clicked button calls playRound function
-// playRound return value is logged to console
-
-let playerSelection;
-
 const rockBtn = document.querySelector('#rock-btn');
 rockBtn.addEventListener('click', () => {
-playerSelection = "rock";
-playRound();
+playRound('rock');
 });
 
 const paperBtn = document.querySelector('#paper-btn');
 paperBtn.addEventListener('click', () => {
-  playerSelection = "paper";
-  console.log(playerSelection);
+  playRound('paper');
 });
 
 const scissorsBtn = document.querySelector('#scissors-btn');
 scissorsBtn.addEventListener('click', () => {
-  playerSelection = "scissors";
-  console.log(playerSelection);
+  playRound('scissors');
 });
 
 function playRound(playerSelection, computerSelection) {
 
 computerSelection = computerPlay();
+const roundResult = document.querySelector('#round-result');
+const playerPoints = document.querySelector('#player-points');
+const computerPoints = document.querySelector('#computer-points');
+const winnerAnnouncement = document.querySelector('#winner-announcement');
+winnerAnnouncement.style.color = "red";
 
 if (playerSelection == computerSelection) {
-  console.log(`It's a draw! Try again. ¯\_(ツ)_/¯ \nYour Score: ${playerScore} | Computer's Score: ${computerScore}`);
+  roundResult.textContent = "It's a draw! Try again. ¯\_(ツ)_/¯";
+  playerPoints.textContent = `${playerScore}`;
+  computerPoints.textContent = `${computerScore}`;
 } else if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper") {
   playerScore++;
-  console.log(`You win, ${playerSelection} beats ${computerSelection}! ᕕ( ᐛ )ᕗ \nYour Score: ${playerScore} | Computer's Score: ${computerScore}`)
+  playerPoints.textContent = `${playerScore}`;
+  computerPoints.textContent = `${computerScore}`;
+  roundResult.textContent = `You win, ${playerSelection} beats ${computerSelection}! ᕕ( ᐛ )ᕗ`;
 } else if (playerSelection == "rock" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "rock") {
   computerScore++;
-  console.log(`You lose, ${computerSelection} beats ${playerSelection}! (┛✧Д✧))┛彡┻━┻ \nYour Score: ${playerScore} | Computer's Score: ${computerScore}`);
-} else if (playerSelection != "rock" || playerSelection != "paper" || playerSelection != "scissors") {
-  console.log(`That is not a valid input, stop cheating! (; ･\`д･´) \nYour Score: ${playerScore} | Computer's Score: ${computerScore}`);
+  roundResult.textContent = `You lose, ${computerSelection} beats ${playerSelection}! (┛✧Д✧))┛彡┻━┻`;
+  playerPoints.textContent = `${playerScore}`;
+  computerPoints.textContent = `${computerScore}`;
+} 
+
+if (playerScore == 5) {
+  winnerAnnouncement.textContent = "You're the Rock, Paper, Scissors champion! o(〃＾▽＾〃)o";
+} else if (computerScore == 5) {
+  winnerAnnouncement.textContent = "You lost to the computer! How embarassing. ((´д｀))";
 }
 }
-
-
-
-
-
-// Plays 5 rounds, then returns an alert with winner and results
-function game() {
-for (let i= 0; i < 5; i++) {
-  playRound();
-}
-if (playerScore > computerScore) {
-  alert(`You\'re the Rock-Paper-Scissors champion! o͡͡͡͡͡͡͡͡͡͡͡͡͡͡╮(｡ᐤヮᐤ｡)╭o͡͡͡͡͡͡͡͡͡͡͡͡͡͡' \n Final Score: You: ${playerScore} | Computer: ${computerScore}`);
-} else if (playerScore < computerScore) {
-  alert(`You lost to the computer! How embarrassing. \(´Д｀) \n Final Score: You: ${playerScore} | Computer: ${computerScore}`);
-} else alert(`There's no winner here! Maybe try again? 「\(°ヘ°) \n Final Score: You: ${playerScore} | Computer: ${computerScore}`)
-}
-
-//game();
 
