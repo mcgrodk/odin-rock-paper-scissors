ec70093b46157e91ffa7e455185a5a4b350e9073
Kyle <mcgroderk@gmail.com>
2022-03-25 14:11:11 +0900

    Rearranged script, added try again, added reset to events

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const rockBtn = document.querySelector('#rock-btn');
+const paperBtn = document.querySelector('#paper-btn');
+const scissorsBtn = document.querySelector('#scissors-btn');
+const roundResult = document.querySelector('#round-result');
+const playerPoints = document.querySelector('#player-points');
+const computerPoints = document.querySelector('#computer-points');
+const winnerAnnouncement = document.querySelector('#winner-announcement');
+const tryAgain = document.querySelector('#try-again');
+
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
   const number = Math.floor(Math.random()*3) + 1;
   if (number == 1) {
@@ -6,57 +18,58 @@ function computerPlay() {
   else if (number == 2) {
       return "paper";
   }
-  else (number == 3) 
-      return "scissors";
+  else return "scissors";
   }
-
-let playerScore = 0;
-let computerScore = 0;
-
-const rockBtn = document.querySelector('#rock-btn');
+  
 rockBtn.addEventListener('click', () => {
-playRound('rock');
+  winnerAnnouncement.textContent = '';
+  tryAgain.textContent = '';
+  playRound('rock');
 });
 
-const paperBtn = document.querySelector('#paper-btn');
 paperBtn.addEventListener('click', () => {
+  winnerAnnouncement.textContent = '';
+  tryAgain.textContent = '';
   playRound('paper');
 });
 
-const scissorsBtn = document.querySelector('#scissors-btn');
 scissorsBtn.addEventListener('click', () => {
+  winnerAnnouncement.textContent = '';
+  tryAgain.textContent = '';
   playRound('scissors');
 });
 
 function playRound(playerSelection, computerSelection) {
 
 computerSelection = computerPlay();
-const roundResult = document.querySelector('#round-result');
-const playerPoints = document.querySelector('#player-points');
-const computerPoints = document.querySelector('#computer-points');
-const winnerAnnouncement = document.querySelector('#winner-announcement');
-winnerAnnouncement.style.color = "red";
 
 if (playerSelection == computerSelection) {
   roundResult.textContent = "It's a draw! Try again. ¯\_(ツ)_/¯";
   playerPoints.textContent = `${playerScore}`;
   computerPoints.textContent = `${computerScore}`;
 } else if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper") {
+  roundResult.textContent = `You win, ${playerSelection} beats ${computerSelection}! ᕕ( ᐛ )ᕗ`;
   playerScore++;
   playerPoints.textContent = `${playerScore}`;
   computerPoints.textContent = `${computerScore}`;
-  roundResult.textContent = `You win, ${playerSelection} beats ${computerSelection}! ᕕ( ᐛ )ᕗ`;
 } else if (playerSelection == "rock" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "rock") {
-  computerScore++;
   roundResult.textContent = `You lose, ${computerSelection} beats ${playerSelection}! (┛✧Д✧))┛彡┻━┻`;
+  computerScore++;
   playerPoints.textContent = `${playerScore}`;
   computerPoints.textContent = `${computerScore}`;
 } 
 
 if (playerScore == 5) {
   winnerAnnouncement.textContent = "You're the Rock, Paper, Scissors champion! o(〃＾▽＾〃)o";
-} else if (computerScore == 5) {
-  winnerAnnouncement.textContent = "You lost to the computer! How embarassing. ((´д｀))";
-}
+  tryAgain.textContent = "Click a button to reset and try again!"
+  playerScore = 0;
+  computerScore = 0;
 }
 
+if (computerScore == 5) {
+  winnerAnnouncement.textContent = "You lost to the computer! How embarassing. ((´д｀))";
+  tryAgain.textContent = "Click a button to reset and try again!"
+  playerScore = 0;
+  computerScore = 0;
+}
+}
